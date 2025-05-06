b0c31fcfd48a8ddbe8869b265e2a57f8c2dd6b7e
Artin Doroudi <GDstudio.plus@gmail.com>
2025-05-05 22:39:24 -0600

    fixed issue with including navbar component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import Navbar from '../components/Navbar'
+import Navbar from '../components/navbar'
 import LoginForm from '../components/LoginForm'
 
 export default function Login() {
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,4 @@
-import Navbar from '../components/Navbar'
+import Navbar from '../components/navbar'
 import RegisterForm from '../components/RegisterForm'
 
 export default function Register() {
